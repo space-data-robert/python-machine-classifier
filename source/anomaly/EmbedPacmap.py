f7945f3b195e2fec727e96b9ce3f50c53632d774
space.data.robert@gmail.com <space.data.robert@gmail.com>
2023-01-03 16:27:27 +0900

    fix. 차원축소 파라미터 수정

--- source/anomaly/EmbedPacmap.py
+++ source/anomaly/EmbedPacmap.py
@@ -3,18 +3,18 @@ from sklearn.base import TransformerMixin, BaseEstimator
 
 
 class EmbedPacmap(BaseEstimator, TransformerMixin):
-    def __init__(self, feature_names):
+    def __init__(self, feature_names, num_iters):
         self.feature_names = feature_names
+        self.num_iters = num_iters
         self.cluster = pacmap.PaCMAP(
-            n_components=len(self.feature_names),
-            n_neighbors=None,
-            MN_ratio=0.5,
-            FP_ratio=2.0,
-            num_iters=10**3,
-            verbose=False,
+            n_components=len(feature_names), 
+            n_neighbors=None, 
+            MN_ratio=0.5, 
+            FP_ratio=2.0, 
+            num_iters=num_iters,
+            verbose=True,
             random_state=27,
-            save_tree=True,
-        )
+            save_tree=True)
 
     def fit(self, X, y=None):
         self.cluster.fit(
