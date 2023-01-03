e5e54fcf08ed1b1687515d57682d356c917ac770
space.data.robert@gmail.com <space.data.robert@gmail.com>
2023-01-03 16:27:01 +0900

    fix. Concat 데이터에서 훈련용셋 추출

--- source/base/MakeDataset.py
+++ source/base/MakeDataset.py
@@ -2,13 +2,16 @@ from sklearn.base import TransformerMixin, BaseEstimator
 
 
 class MakeDataset(BaseEstimator, TransformerMixin):
-    def __init__(self, train_len):
+    def __init__(self, train_len, target_name):
         self.train_len = train_len
+        self.target_name = target_name
 
     def fit(self, X, y=None):
         return self
 
     def transform(self, X):
-        train = X.iloc[:self.train_len, :]
-        valid = X.iloc[self.train_len:, :]
-        return train, valid
+        x_train = X.iloc[:self.train_len, :].copy()
+        y_train = x_train.pop(
+            self.target_name
+        )
+        return x_train, y_train
